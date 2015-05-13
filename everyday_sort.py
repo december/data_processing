b9f6ebcccbc143cd888079c3d64163c938633aa8
Luyf <luyunfeicx@163.com>
2015-05-13 14:05:07 +0800

    Update everyday_sort.py

--- everyday_sort.py
+++ everyday_sort.py
@@ -4,6 +4,11 @@ import sys
 dataset = []
 bestdata = []
 
+def tran_time(old_time):
+	new_time = old_time[0] + old_time[1] + old_time[2] + old_time[3] + '-' + old_time[4] + old_time[5] + '-' + old_time[6] + old_time[7] + ' '
+	new_time += old_time[8] + old_time[9] + ':' + old_time[10] + old_time[11] + ':' + old_time[12] + old_time[13]
+	return new_time
+
 class Everyday:
 	def __init__(self, line):
 		self.imei = line[0]
@@ -19,8 +24,15 @@ class Everyday:
 		self.track_id = line[10]
 		self.type = line[11]
 		self.mf = line[12]
-		self.pack_name = line[13]
-		self.tag_id = line[14]
+		self.toprint = line[0]
+		for i in range(1, 6):
+			self.toprint += ' ' + line[i]
+		self.toprint += ' '
+		self.toprint += tran_time(line[6])
+		for i in range(7, 13):
+			self.toprint += ' ' + line[i]
+		#self.pack_name = line[13]
+		#self.tag_id = line[14]
 	def __cmp__(self, other):
 		if self.imei < other.imei:
 			return -1
@@ -31,23 +43,18 @@ class Everyday:
 				return 0
 		return 1
 
-def tran_time(old_time):
-	new_time = old_time[0] + old_time[1] + '-' + old_time[2] + old_time[3] + '-' old_time[4] + old_time[5] + ' '
-	new_time += old_time[6] + old_time[7] + ':' + old_time[8] + old_time[9] + ':' old_time[10] + old_time[11]
-	return new_time
-
 def readdata(filegroup):
 	for datafile in filegroup:
 		fin = open(datafile, 'r')
 		raw_data = fin.readlines()
 		for temp in raw_data:
-			data = temp.strip().split('\t')
+			data = temp.strip().split('	')
 			item = Everyday(data)
 			dataset.append(item)
 
 def examine(imeinum):
 	for item in bestdata:
-		if item.imei == imeinum:
+		if item[0] == imeinum:
 			return True
 	return False
 
@@ -65,13 +72,14 @@ for i in range(20150201, 20150203):
 readdata(files)
 dataset.sort()
 for i in range(0, 10):
+	temp = []
 	temp.append(str(0))
 	temp.append(int(0))
 	bestdata.append(temp)
 count = 1
 dataset[0].reporttime = tran_time(dataset[i].reporttime)
 for i in range(1, len(dataset)):
-	dataset[i].reporttime = tran_time(dataset[i].reporttime)
+	#dataset[i].reporttime = tran_time(dataset[i].reporttime)
 	if dataset[i].imei == dataset[i-1].imei:
 		count = count + 1
 	else:
@@ -81,17 +89,35 @@ for i in range(1, len(dataset)):
 			bestdata.sort(lambda a, b:int(a[1]) - int(b[1]))
 		count = 1
 for item in bestdata:
-	print item[0] + ' ' + item[1] + '\n'
-fout = open('yyb_everyday_data_sorted', 'w')
+	print item[0] + ' ' + str(item[1]) + '\n'
+prefix = 'everyday_sort/yyb_everyday_data_sorted'
+order = 0
+total = 0
+fout = open(prefix + str(order), 'w')
 for item in dataset:
-	fout.write(item)
+	fout.write(item.toprint)
 	fout.write('\n')
+	total += 1
 	if examine(item.imei):
 		bestdataset.append(item)
+	if total >= 11000000:
+		total = 0
+		order += 1
+		fout.close()
+		fout = open(prefix + str(order), 'w')
 fout.close()
-fout = open('yyb_everyday_data_best', 'w')
+prefix = 'everyday_most/yyb_everyday_data_best'
+order = 0
+fout = open(prefix + str(order), 'w')
+lastitem = dataset[0]
+lastitem.imei = 0
 for item in bestdataset:
-	fout.write(item)
+	if lastitem.imei != item.imei:
+		fout.close()
+		order += 1
+		fout = open(prefix + str(order), 'w')
+		lastitem.imei = item.imei
+	fout.write(item.toprint)
 	fout.write('\n')
 fout.close()
 
